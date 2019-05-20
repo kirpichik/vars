d8476402ab5a9ef64b95f209ad90b8e0b9e60ddd
Kirill Polushin <kirill@polushin.org>
2019-05-20 20:40:46 +0700

    Implement import statement scope apply

--- src/main/scala/org/polushin/vars/parser/VarsImportStatement.scala
+++ src/main/scala/org/polushin/vars/parser/VarsImportStatement.scala
@@ -2,10 +2,23 @@ package org.polushin.vars.parser
 
 import org.polushin.vars.Scope
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 class VarsImportStatement(factory: ParserFactory, filename: String) extends Statement {
 
-  override def applyScope(scope: Scope): Try[Scope] = ???
+  override def applyScope(scope: Scope): Try[Scope] = factory.parseFile(filename) match {
+    case Failure(exception) => Failure(exception)
+    case Success(parser) => withResources(parser)(parser => applyParserScope(parser, scope))
+  }
+
+  private def applyParserScope(parser: Parser, scope: Scope): Try[Scope] = {
+    parser.nextStatement() match {
+      case Failure(exception) => Failure(exception)
+      case Success(optStatement) => optStatement match {
+        case None => Success(scope)
+        case Some(statement) => statement.applyScope(scope); applyParserScope(parser, scope)
+      }
+    }
+  }
 
 }
